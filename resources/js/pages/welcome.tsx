23768459b3a338e9a8314f0df746014637ff272b
Aljon Sumilhig <33595791+assumilhig@users.noreply.github.com>
2025-08-23 16:55:48 +0800

    Initial commit

---
+++ resources/js/pages/welcome.tsx
@@ -0,0 +1,10 @@
+import { Head } from '@inertiajs/react';
+
+export default function Welcome() {
+    return (
+        <>
+            <Head title="Welcome" />
+            <h3>Welcome</h3>
+        </>
+    );
+}
